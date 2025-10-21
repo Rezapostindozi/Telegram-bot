068a84760afa02affccbfdb4088da199749954cb
rezapostindozi <rezapostindoziii@gmail.com>
2025-10-21 00:24:20 -0700

    Update main.py

--- mybot/main.py
+++ mybot/main.py
@@ -4,14 +4,11 @@ from handlers.start_handler import register_handlers as register_start_handlers
 from handlers.product_handler import register_handlers as register_product_handlers
 from handlers.subscription_handler import register_handlers as register_subscription_handlers
 
-# ایجاد و آماده‌سازی دیتابیس
 init_db()
 
-# توکن ربات تلگرام (دسترسی شخصی خود را جایگزین کنید)
-BOT_TOKEN = "7949112582:AAHUS0d19fvKn0G3eGo6MzdDbBIiBAibES8"
+BOT_TOKEN = ""
 bot = TeleBot(BOT_TOKEN)
 
-# ثبت هندلرها
 register_start_handlers(bot)
 register_product_handlers(bot)
 register_subscription_handlers(bot)
